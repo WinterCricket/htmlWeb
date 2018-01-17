ea32c65e59f3c5ecc9c2ea2b7e6f9a252c3d32c7
WinterCricket <hodhodhod2003@gmail.com>
2018-01-17 02:00:51 -0600

    clean up the js

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,6 @@ function listMentors() {
 	
 	var list = "<ul><li>Wesley McNair</li><li>Maud Casey</li><li>Debra Spark</li><li>David Shields</li><li>Michael Parker</li><li>Marianne Boruch</li><li>Pat O'Donnell</li><li>Antonya Nelson</li><li>Charles Baxter</li><li>Jeremy Gavron</li><li>Luljeta Lleshanaku</li></ul>";
  	document.getElementById("mentors").innerHTML = list;
+ 	
 }
+
